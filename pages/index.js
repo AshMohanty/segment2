6f58cd759cfdd7318e20d6e31a95258a740523b9
AshMohanty <mohantay@gmail.com>
2021-01-29 12:22:13 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,12 @@ const Home = ({ inventoryData = [], categories: categoryData = [] }) => {
           <title>Jamstack ECommerce</title>
           <meta name="description" content="Jamstack ECommerce Next provides a way to quickly get up and running with a fully configurable ECommerce site using Next.js." />
           <meta property="og:title" content="Jamstack ECommerce" key="title" />
+          <script>
+            !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.13.1";
+            analytics.load("m0WjgIVxj6mMK61osLMpjsZkpDCRFbYM");
+            analytics.page();
+          }}();
+        </script>
         </Head>
         <div className="bg-blue-300
         p-6 pb-10 smpb-6
